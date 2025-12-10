cb6e459ff4c220062aa3aa818dbf5a2b30e96bba
TruthMarket <6tlod-wtmld-tf74b-bp7pp-6urmn-hmg5d-cbzaj-4derq-bmgnl-tedfk-aae@dmail.ai>
2025-12-10 21:49:03 +0800

    change env

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { DEFAULT_SCOPE } from './config/sync'
 async function main() {
   try {
     const writeToSupabase = true
-    const reSet = true
+    const reSet = false
     const isUpdateSyncBlock = false
     let last_synced_block = 14458354 // 重新开始同步
 
@@ -26,26 +26,26 @@ async function main() {
     }
 
 
-    // const result_truthBox = await fetchTruthBoxEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
+    const result_truthBox = await fetchTruthBoxEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
 
-    // const result_truthNFT = await fetchTruthNFTEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
+    const result_truthNFT = await fetchTruthNFTEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
 
     const result_exchange = await fetchExchangeEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
 
-    // const result_fundManager = await fetchFundManagerEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
+    const result_fundManager = await fetchFundManagerEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
 
-    // const result_userId = await fetchUserIdEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
+    const result_userId = await fetchUserIdEvents(DEFAULT_SCOPE, last_synced_block, writeToSupabase)
 
-    // const latest_block = Math.min(
-    //   result_truthBox.block_number,
-    //   result_truthNFT.block_number,
-    //   result_exchange.block_number,
-    //   result_fundManager.block_number,
-    //   result_userId.block_number,
-    // )
-    // if (isUpdateSyncBlock) {
-    //   await updateSyncStatus(DEFAULT_SCOPE, latest_block)
-    // }
+    const latest_block = Math.min(
+      result_truthBox.block_number,
+      result_truthNFT.block_number,
+      result_exchange.block_number,
+      result_fundManager.block_number,
+      result_userId.block_number,
+    )
+    if (isUpdateSyncBlock) {
+      await updateSyncStatus(DEFAULT_SCOPE, latest_block)
+    }
 
   } catch (error) {
     console.error('❌ 查询事件失败：', error)
